8139d17a6829a846d8c17280d6ab631f3c7abdef
Aliaksei Hryhoryeu <aliaksei.hryhoryeu.1@gmail.com>
2024-10-14 19:36:55 +0200

    added auth screens

--- android/app/src/main/java/com/example/telegramclone/MainActivity.kt
+++ android/app/src/main/java/com/example/telegramclone/MainActivity.kt
@@ -3,15 +3,14 @@ package com.example.telegramclone
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 //        setContentView(R.layout.activity_main)
-        setContentView(R.layout.login)
+          setContentView(R.layout.signup)
+//        setContentView(R.layout.login)
 
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
 //            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
