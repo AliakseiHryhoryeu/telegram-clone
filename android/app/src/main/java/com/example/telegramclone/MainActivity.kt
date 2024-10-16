c44fe455f710ae93e44050373b05ee200dd7463b
Aliaksei Hryhoryeu <aliaksei.hryhoryeu.1@gmail.com>
2024-10-16 09:06:30 +0200

    add login screen state

--- android/app/src/main/java/com/example/telegramclone/MainActivity.kt
+++ android/app/src/main/java/com/example/telegramclone/MainActivity.kt
@@ -8,18 +8,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        setContentView(R.layout.page404)
+
+    }
+}
+
 //        setContentView(R.layout.activity_main)
 //        setContentView(R.layout.signup)
 //        setContentView(R.layout.login)
-        //       setContentView(R.layout.resetpassword)
-        //       setContentView(R.layout.page404)
-        setContentView(R.layout.page404)
+//       setContentView(R.layout.resetpassword)
+//       setContentView(R.layout.page404)
 
-
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
-    }
-}
